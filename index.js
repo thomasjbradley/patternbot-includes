19b837661c46f2253d74b8afa51914e91f83a78a
Thomas J Bradley <hey@thomasjbradley.ca>
2018-02-18 10:58:40 -0500

    Add the first version of the code

---
+++ index.js
@@ -0,0 +1,230 @@
+/**
+ * Patternbot includes
+ * https://github.com/thomasjbradley/patternbot-includes
+ * @author Thomas J Bradley <hey@thomasjbradley.ca>
+ * @copyright 2018 Thomas J Bradley
+ * @license MIT
+ */
+const patternBotIncludes = function (manifest) {
+  'use strict';
+
+  const findRootPath = function () {
+    const rootMatcher = /\/common\//;
+    const allScripts = document.querySelectorAll('script:not([type])');
+    let t = allScripts.length, i = 0;
+
+    for (i = 0; i < t; i++) {
+      if (rootMatcher.test(allScripts[i].src)) {
+        return allScripts[i].src.split(rootMatcher)[0];
+        break;
+      }
+    }
+  };
+
+  const addCssFile = function (href) {
+    const newLink = document.createElement('link');
+
+    newLink.setAttribute('rel', 'stylesheet');
+    newLink.setAttribute('href', href);
+    document.head.appendChild(newLink);
+  }
+
+  const bindAllCssFiles = function (rootPath) {
+    if (manifest.commonInfo && manifest.commonInfo.readme && manifest.commonInfo.readme.attributes &&  manifest.commonInfo.readme.attributes.fontUrl) {
+      addCssFile(manifest.commonInfo.readme.attributes.fontUrl);
+    }
+
+    if (manifest.patternLibFiles && manifest.patternLibFiles.commonParsable) {
+      if (manifest.patternLibFiles.commonParsable.modulifier) addCssFile(`..${manifest.config.commonFolder}/${manifest.config.commonParsableFilenames.modulifier}`);
+      if (manifest.patternLibFiles.commonParsable.gridifier) addCssFile(`..${manifest.config.commonFolder}/${manifest.config.commonParsableFilenames.gridifier}`);
+      if (manifest.patternLibFiles.commonParsable.typografier) addCssFile(`..${manifest.config.commonFolder}/${manifest.config.commonParsableFilenames.typografier}`);
+      if (manifest.patternLibFiles.commonParsable.theme) addCssFile(`..${manifest.config.commonFolder}/${manifest.config.commonParsableFilenames.theme}`);
+    }
+
+    manifest.userPatterns.forEach((pattern) => {
+      if (!pattern.css) return;
+
+      pattern.css.forEach((css) => {
+        addCssFile(`../${css.localPath}`);
+      });
+    });
+  };
+
+  const getPatternInfo = function (patternElem) {
+    let patternInfoJson;
+
+    try {
+      patternInfoJson = JSON.parse(patternElem.innerText);
+      return patternInfoJson;
+    } catch (e) {
+      console.group('JSON error in pattern include');
+      console.log(patternElem.dataset.pattern);
+      console.log(patternElem.innerText);
+      console.log(e);
+      console.groupEnd();
+      return {};
+    }
+  };
+
+  const getPatternUrl = function (rootPath, patternName) {
+    patternName = (patternName.slice(-5) === '.html') ? patternName.slice(0, -5) : patternName;
+    return `${rootPath}/patterns/${patternName}.html`;
+  };
+
+  const constructPattern = function (rootPath, patternElem) {
+    return {
+      name: patternElem.dataset.pattern,
+      url: getPatternUrl(rootPath, patternElem.dataset.pattern),
+      html: false,
+      elem: false,
+    };
+  };
+
+  const htmlStringToElem = function (html) {
+    const doc = (new DOMParser()).parseFromString(html, 'text/html');
+    return doc.body.firstElementChild;
+  };
+
+  const replaceElementValue = function (elem, sel, data) {
+    elem.innerText = data;
+  };
+
+  const replaceAttributeValue = function (elem, sel, data) {
+    const isXlinkAttr = /xlink\:/.test(sel);
+    const attr = sel.match(/\[([^\]]+)\]$/)[1];
+
+    if (isXlinkAttr) {
+      elem.setAttributeNS('http://www.w3.org/1999/xlink', attr, data);
+    } else {
+      elem.setAttribute(attr, data);
+    }
+  };
+
+  const outputPattern = function (patternElem, patternDetails) {
+    let patternOutElem, patternData;
+
+    if (!patternDetails.html) return;
+
+    patternOutElem = htmlStringToElem(patternDetails.html);
+    patternData = getPatternInfo(patternElem);
+
+    Object.keys(patternData).forEach((sel) => {
+      const isAttributeSelector = /\[.*\]/.test(sel);
+      const jsSel = sel.replace(/\[.*:/, '[*|');
+      const elem = patternOutElem.querySelector(jsSel);
+
+      if (isAttributeSelector) {
+        replaceAttributeValue(elem, sel, patternData[sel]);
+      } else {
+        replaceElementValue(elem, sel, patternData[sel]);
+      }
+    });
+
+    patternElem.parentNode.replaceChild(patternOutElem, patternElem);
+  };
+
+  const showLoadingScreen = function () {
+
+  };
+
+  const hideLoadingScreen = function () {
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+
+      });
+    });
+  };
+
+  const findAllPatternTags = function () {
+    return document.querySelectorAll('script[type="application/json"][data-pattern]');
+  };
+
+  const constructAllPatterns = function (rootPath, allPatternTags) {
+    let allPatterns = {};
+
+    [].forEach.call(allPatternTags, (patternElem) => {
+      if (allPatterns[patternElem.dataset.pattern]) return;
+
+      allPatterns[patternElem.dataset.pattern] = constructPattern(rootPath, patternElem);
+    });
+
+    return allPatterns;
+  };
+
+  const buildPatternFetchPromises = function (allPatternUrls) {
+    return allPatternUrls.map((url) => {
+      return new Promise((resolve, reject) => {
+        fetch(url).then((resp) => {
+          if (resp.status >= 200 && resp.status <= 299) {
+            return resp.text();
+          } else {
+            console.group('Download error');
+            console.log(resp.url);
+            console.log(`Error ${resp.status}: ${resp.statusText}`);
+            console.groupEnd();
+            return '';
+          }
+        }).then(function (html) {
+          resolve(html);
+        }).catch((e) => {
+          console.group('Download error');
+          console.log(url);
+          console.log(e);
+          console.groupEnd();
+          resolve('');
+        });
+      });
+    });
+  };
+
+  const loadAllPatterns = function (allPatterns) {
+    let patternUrls = {};
+    let allPatternKeys = Object.keys(allPatterns);
+
+    allPatternKeys.forEach((patternKey) => {
+      patternUrls[patternKey] = allPatterns[patternKey].url;
+    });
+
+    return new Promise((resolve, reject) => {
+      Promise.all(buildPatternFetchPromises(Object.values(patternUrls))).then((allPatternsHtml) => {
+        allPatternsHtml.forEach((html, i) => {
+          allPatterns[allPatternKeys[i]].html = html;
+        });
+        resolve(allPatterns);
+      }).catch((e) => {
+        console.group('Pattern load error');
+        console.log(e);
+        console.groupEnd();
+        resolve(allPatterns);
+      });
+    });
+  };
+
+  const renderAllPatterns = function (allPatternTags, allLoadedPatterns) {
+    [].forEach.call(allPatternTags, (elem) => {
+      outputPattern(elem, allLoadedPatterns[elem.dataset.pattern]);
+    });
+  };
+
+  const init = function () {
+    let rootPath, patternsToLoad, allPatternTags = [], allPatterns = [];
+
+    showLoadingScreen();
+
+    rootPath = findRootPath();
+    bindAllCssFiles(rootPath);
+    allPatternTags = findAllPatternTags();
+    allPatterns = constructAllPatterns(rootPath, allPatternTags);
+
+    loadAllPatterns(allPatterns).then((allLoadedPatterns) => {
+      renderAllPatterns(allPatternTags, allLoadedPatterns);
+      hideLoadingScreen();
+    }).catch((e) => {
+      console.group('Pattern load error');
+      console.log(e);
+      console.groupEnd();
+    });
+  };
+
+  init();
+};
